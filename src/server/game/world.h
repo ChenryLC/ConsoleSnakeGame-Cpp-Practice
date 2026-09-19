fcd04e4eace3615f76d18f6c70e7010e14afad49
SteveChenry <2295764501@qq.com>
2026-09-19 22:48:16 +0800

    feat: rewrite declare of class World

--- src/server/game/world.h
+++ src/server/game/world.h
@@ -1,58 +1,43 @@
 #pragma once
+#include "../event/game_event.h"
+#include "snake.h"
 #include <vector>
 #include <unordered_set>
 #include <random>
 #include <memory>
-#include "../../common/constant.h"
-#include "snake.h"
-#include "../../common/chunk.h"
-#include "../error/game_error.h"
-#include "../event/game_event.h"
+#include <functional>
 
+class SnakeManager;
+class ChunkManager;
 class World{
-    private:
-    
-    unsigned map_size;//以区块计
-    std::vector<int> idList;
-    std::vector<Snake> snakeList;
-    std::unordered_map<ChunkPos,Chunk> chunkList;
-    std::vector<ChunkPos> chunkUpdateList;
-    std::unordered_map<int, Direction> playerDirection;
-    std::unordered_map<int, ChunkPos> chunkPosOfPlayer;
+private:
+    SnakeManager snakes;
+    ChunkManager chunks;
 
-    std::vector<ChunkPos> dirtyChunkList;
+    size_t step;
 
-    std::mt19937 random_engine;
-
-    std::shared_ptr<IGameEventHandler> game_event_handler;
-
-    size_t step_count=0;
-    
-    int playerJoin(int playerId_);
-    void addFood(ChunkPos);
-    void chunkUpdate();
-
-    public:
-
-    World(int sz_, std::vector<int> playerList, std::shared_ptr<IGameEventHandler> =nullptr);
-
+public:
+    World(int size, std::vector<int> players);
 
     void step();
-    bool playerLeave(int playerId_);
 
-    int getMapSize() const{return map_size*CHUNK_SIZE;}
-    size_t getCurrentStep() const{return step_count;}
+    bool setSnakeDirection(int id, Direction dir);
 
-    const Position& getPlayerHeadPos(int) const;
-    const Chunk& getChunk(const ChunkPos&) const;
-    const std::optional<Snake> getSnake(int) const;
-    const std::vector<ChunkPos>& getDirtyChunks() const{return dirtyChunkList;}
-    std::vector<ChunkPos> getLoadedChunkPos() const;
+    std::vector<int> getSnakeList();
+    std::vector<int> getAilveSnakeList();
+    Direction getSnakeDirection(int id);
+    SnakeStatus getSnakeStatus(int id);
+    Snake::body_type getSnakeBody(int id);
+    Position getSnakeHeadPos(int id);
 
-    std::vector<int> getActivePlayerIdList() const;
-    std::vector<int> getAllPlayerIdList() const;
-    bool isPlayerAlive(int) const;
+    size_t getStepCount() const;
+    size_t getMapSize() const;
+    std::optional<std::reference_wrapper<std::unordered_set<Position>>> getSnakeChunk(ChunkPos);
+    std::optional<std::reference_wrapper<std::unordered_set<Position>>> getFoodChunk(ChunkPos);
 
-    bool setPlayerDirection(int, Direction);
-    std::unordered_map<int, bool> setMultiplePlayerDirection(const std::unordered_map<int, Direction>);
+    const auto& getToSnake() const;
+    const auto& getToFood() const;
+    const auto& getToAir() const;
+
+    const std::vector<Position> getAllHeadPos() const;
 };
